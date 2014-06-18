90fb458aa2b1b26cecd26747f4dee8a56374964f
stefanocudini <stefano.cudini@gmail.com>
2014-06-18 20:36:19 +0200

    item users

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+/*
+<osm version="0.6" generator="OpenStreetMap server">
+<user id="130691" display_name="StefanoCudini" account_created="2009-06-01T18:03:32Z">
+<description>[labs.easyblog.it](http://labs.easyblog.it/)</description>
+<contributor-terms agreed="true"/>
+<img href="http://api.openstreetmap.org/attachments/users/images/000/130/691/original/39c24d8cd7d1cb787df7f31ffd98c2c9.jpg"/>
+<roles></roles>
+<changesets count="1804"/>
+<traces count="125"/>
+<blocks>
+<received count="0" active="0"/>
+</blocks>
+</user>
+</osm>	
+*/
 $(function() {
 
 var minZoom = 15,
@@ -15,52 +30,75 @@ var geoLayer = L.geoJson([]).addTo(map);
 
 var users = {};
 
+var sidebar$ = $('#sidebar'),
+	userlist$ = $('#userlist');
+
 window.users = users;
+
+function avatarByUid(uid) {
+	var avatar = '';
+	
+	sidebar$.addClass('loading');
+	
+	$.ajax({
+		async: false,
+		url: 'http://api.openstreetmap.org/api/0.6/user/'+uid,
+		dataType: 'xml',
+		success: function(xml) {
+			avatar = $(xml).find('img').attr('href');
+			sidebar$.removeClass('loading');
+		}
+	});
+	return avatar;
+}
 
 L.layerJSON({
 	//http://overpass-turbo.eu/s/3Ml
 	//(node();way["highway"~"."]();>;);out meta;
 	//way["highway"~"."]({lat1},{lon1},{lat2},{lon2});>;out meta;
+	minZoom: 15,
 	url: overpassUrl+'data=[out:json];way["highway"~"."]({lat1},{lon1},{lat2},{lon2});>;out meta;',
 	propertyItems: 'elements',
 	propertyTitle: 'tags.name',
 	propertyLoc: ['lat','lon'],
 	dataToMarker: function(data,ll) {
 		//console.log(data);
-		if(!users[data.user])
-			users[data.user] = [];
+		if(!users[data.uid])
+		{
+			users[data.uid] = {
+				username: data.user,
+				avatar: avatarByUid(data.uid),
+				color: rgb2hex(randcolor()),
+				locs: [ll]
+			};
+			userlist$.append('<div class="useritem" style="background:'+users[data.uid].color+'">'+
+				'<img height="24" width="25" src="'+users[data.uid].avatar+'" /> '+
+				'<a target="_blank" href="http://osm.org/user/'+users[data.uid].username+'">'+users[data.uid].username+'</a>'+
+				'</div>');
+		}
 		else
-			users[data.user].push(ll);
+			users[data.uid].locs.push(ll);
 
 		return false;//L.circle(ll, 10);
 	}
 })
-// .on('dataloading',function(e) {
-// 	console.log('dataloading',e);
-// 	//loader.style.display = 'block';
-// })
+.on('dataloading',function(e) {
+	console.log('dataloading');
+	userlist$.empty();
+})
 .on('dataloaded',function(e) {
-	//console.log('dataloaded', users);
-	//loader.style.display = 'none';
-	
-	for(var username in users)
+	for(var uid in users)
 	{
-		var color = rgb2hex(randcolor());
-
 		//TODO avatar http://leafletjs.com/reference.html#imageoverlay
-		
-		var rect = L.rectangle(L.latLngBounds(users[username]), {color: color }).addTo(map);
-		delete users[username];
-		
-		console.log(username, color, L.Util.stamp(rect) );
-
+		var rect = L.rectangle(L.latLngBounds(users[uid].locs), {color: users[uid].color }).addTo(map);
 		//geoLayer.addData( rect.toGeoJSON() );
 	}
+	
 })
 .addTo(map);
 
 //CONTROL SIDEBAR
-//L.control.sidebar('sidebar',{position:'right', autoPan:false}).addTo(map).show();
+L.control.sidebar('sidebar',{position:'right'}).addTo(map).show();
 
 });
 
